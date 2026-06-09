04eda71e7bb6ebbb111459846bc228aac17c934a
Daro <darojas2k01@gmail.com>
2026-06-08 20:17:12 -0600

    Agregado los service y los endspoints en el controller

--- src/main/java/ucr/ac/cr/StayPeriod/controller/PublicationController.java
+++ src/main/java/ucr/ac/cr/StayPeriod/controller/PublicationController.java
@@ -1,4 +1,77 @@
 package ucr.ac.cr.StayPeriod.controller;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.FieldError;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.*;
+import ucr.ac.cr.StayPeriod.model.DTO.PublicationDTO;
+import ucr.ac.cr.StayPeriod.model.Publication;
+import ucr.ac.cr.StayPeriod.service.PublicationService;
+import java.util.HashMap;
+import java.util.Map;
 
+@RequestMapping("api/user")
+@RestController
 public class PublicationController {
+    @Autowired
+    PublicationService service;
+
+     @GetMapping
+    public ResponseEntity<?> findAll (){return ResponseEntity.ok(service.findAll());}
+
+
+     @GetMapping("/{id}")
+    public ResponseEntity<?> findById (@PathVariable Integer id) {
+         PublicationDTO dto = this.service.findById(id);
+         if (dto != null) {
+             return ResponseEntity.status(HttpStatus.FOUND).body("Publicacion encontrada: \n" + dto.toString());
+         }
+         return ResponseEntity.noContent().build();
+     }
+
+     @PostMapping ("/save")
+     public ResponseEntity<?> savePublication (@Validated @RequestBody Publication publication, BindingResult result) {
+         if (result.hasErrors()) {
+             /*Contenedor de errores*/
+             Map<String, String> errors = new HashMap<>();
+             /*buscamos los errores*/
+             for (FieldError error : result.getFieldErrors()) {
+                 /*metemos los errores en el contenedor*/
+                 errors.put(error.getField(), error.getDefaultMessage());
+             }
+             /*retornamos el mensaje con los errores*/
+             return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(errors);
+         }
+
+         PublicationDTO newDtoPublication = this.service.savePublication(publication);
+            if (newDtoPublication != null) {
+                return ResponseEntity.status(HttpStatus.CREATED).body("Publicacion creada: \n" + newDtoPublication.toString());
+            }
+            return ResponseEntity.status(HttpStatus.CONFLICT).body("La Publicacion " + publication.getId() + " ya existe");
+     }
+
+     @PatchMapping ("/update/{id}")
+     public ResponseEntity<?> updatePublicationById (@PathVariable Integer id, @Validated @RequestBody Publication updatedPublication, BindingResult result) {
+         if (result.hasErrors()) {
+             Map<String, String> errors = new HashMap<>();
+             for (FieldError error : result.getFieldErrors()) {
+                 errors.put(error.getField(), error.getDefaultMessage());
+             }
+             return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(errors);
+         }
+         PublicationDTO foundedDto = this.service.updatePublicationById(id, updatedPublication);
+         if (foundedDto != null) {
+             return ResponseEntity.status(HttpStatus.OK).body("Publicacion actualizada: \n" + foundedDto.toString());
+         }
+         return ResponseEntity.status(HttpStatus.CONFLICT).body("La Publicacion con id " + id + " no existe");
+     }
+
+     @DeleteMapping("/delete/{id}")
+     public ResponseEntity<?> deletePublicationById (@PathVariable Integer id) {
+         this.service.deletePublication(id);
+         return ResponseEntity.noContent().build();
+     }
+
 }
--- src/main/java/ucr/ac/cr/StayPeriod/service/RequestService.java
+++ src/main/java/ucr/ac/cr/StayPeriod/service/RequestService.java
@@ -1,10 +1,86 @@
 package ucr.ac.cr.StayPeriod.service;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import ucr.ac.cr.StayPeriod.model.DTO.RequestDTO;
+import ucr.ac.cr.StayPeriod.model.Request;
 import ucr.ac.cr.StayPeriod.repository.RequestRepository;
+
+import java.util.List;
+import java.util.Optional;
 
 @Service
 public class RequestService {
     @Autowired
     RequestRepository repository;
+
+    public RequestDTO convertRequestToDTO (Request request){
+        RequestDTO requestDTO = new RequestDTO();
+        requestDTO.setRequest_id(request.getId());
+        requestDTO.setRequest_startDate(request.getStartDate());
+        requestDTO.setRequest_endDate(request.getEndDate());
+        requestDTO.setRequest_user_id(request.getApplicant().getId());
+        requestDTO.setRequest_rental_id(request.getRental().getId());
+        return requestDTO;
+    }
+
+    public List<RequestDTO> convertListToDTO (List<Request> requestList){
+        List<RequestDTO> dtoList = new java.util.ArrayList<>();
+        if(requestList.isEmpty()){
+           return null;
+        }
+        for(Request request: requestList){
+            dtoList.add(convertRequestToDTO(request));
+            return dtoList;
+        }
+        return null;
+    }
+
+    //metodo para encontrar la lista entera
+    public List<RequestDTO> findAll () {
+        return this.convertListToDTO(this.repository.findAll());
+    }
+    //metodo para encontrar una solicitud por id
+    public RequestDTO findById (Integer id) {
+        java.util.Optional<Request> opt = this.repository.findById(id);
+        if (opt.isPresent()) {
+            return this.convertRequestToDTO(opt.get());
+        }
+        return null;
+    }
+
+    //metodo para crear una solicitud
+    public RequestDTO saveRequest (Request request) {
+        java.util.Optional<Request> opt = this.repository.findById(request.getId());
+        if (opt.isPresent()) {
+            return null;
+        }
+        return this.convertRequestToDTO(this.repository.save(request));
+    }
+
+    //metodo para actualizar una solicitud por id
+    public RequestDTO updateRequestById (Integer id, Request request) {
+        Optional<Request> opt = this.repository.findById(id);
+        if (opt.isPresent()) {
+            Request requestToUpdate = opt.get();
+            requestToUpdate.setStartDate(request.getStartDate());
+            requestToUpdate.setEndDate(request.getEndDate());
+            requestToUpdate.setApplicant(request.getApplicant());
+            requestToUpdate.setRental(request.getRental());
+            return this.convertRequestToDTO(this.repository.save(requestToUpdate));
+        }
+        return null;
+    }
+
+    //metodo para eliminar una solicitud
+
+    public void deleteRequestById (Integer id) {
+        Optional<Request> opt = this.repository.findById(id);
+        if (opt.isPresent()) {
+            this.repository.deleteById(id);
+        }
+    }
+
+
+
+
 }
